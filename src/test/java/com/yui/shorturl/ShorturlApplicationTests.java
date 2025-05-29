c1023c130f5f296ba2c50bf92ac356ad22c2fcd8
rianfernandesno <zrpapz12@gmail.com>
2025-05-29 20:06:53 -0300

    Project Created

---
+++ src/test/java/com/yui/shorturl/ShorturlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
