50f1b6328b718865d6355b8412af56a4788fa7d0
rianfernandesno <zrpapz12@gmail.com>
2025-05-29 21:14:37 -0300

    post url

--- src/main/java/com/yui/shorturl/controllers/UrlController.java
+++ src/main/java/com/yui/shorturl/controllers/UrlController.java
@@ -1,9 +1,25 @@
 package com.yui.shorturl.controllers;
 
+import com.yui.shorturl.models.dto.UrlDTO;
+import com.yui.shorturl.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/urls")
 public class UrlController {
+
+    @Autowired
+    private UrlService service;
+
+    @PostMapping
+    public ResponseEntity<String> postLongUrl(@RequestBody UrlDTO dto){
+        String url = service.save(dto);
+
+        return ResponseEntity.ok().body(url);
+    }
 }
--- src/main/java/com/yui/shorturl/service/UrlService.java
+++ src/main/java/com/yui/shorturl/service/UrlService.java
@@ -1,8 +1,35 @@
 package com.yui.shorturl.service;
 
+import com.yui.shorturl.models.dto.UrlDTO;
+import com.yui.shorturl.models.entities.Url;
+import com.yui.shorturl.repositories.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.RandomStringUtils;
 
 @Service
 public class UrlService {
 
+    @Autowired
+    private UrlRepository repository;
+
+    public String save(UrlDTO dto){
+        Url url = new Url();
+
+        copy(dto, url);
+
+        repository.insert(url);
+
+        return "http://localhost:8080/urls" + url.getId();
+    }
+
+    private Url copy(UrlDTO dto, Url url){
+        do{
+            url.setId(RandomStringUtils.randomAlphabetic(5, 12));
+        }while (repository.existsById(url.getId()));
+
+        url.setFullUrl(dto.getFullUrl());
+
+        return url;
+    }
 }
